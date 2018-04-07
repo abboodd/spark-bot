13170cc842a6f64e7765a18b78ccca0ccfb1f66c
abboodd <38141887+abboodd@users.noreply.github.com>
2018-04-07 17:40:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -265,7 +265,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` ON ${Rv clan}`,"http://twitch.tv/S-F")
+client.user.setGame('Rv clan} - *help`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 client.on("guildCreate", guild => {
