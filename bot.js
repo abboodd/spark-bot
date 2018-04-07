a22b94b72b13b2746996226e896979c49909592c
abboodd <38141887+abboodd@users.noreply.github.com>
2018-04-07 11:41:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,6 +104,30 @@ client.on('message', message => {
 });
 
 
+client.on("message", message => {
+    var prefix = "&clear";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "&clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم مسح الشات",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل ",
+        footer: {
+          text: "! Rasta#9764"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+});
+
+
 
 
 
