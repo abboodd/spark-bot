b747a1037e0b3998033f90f10ba25bdc83cc99bb
abboodd <38141887+abboodd@users.noreply.github.com>
2018-04-07 11:31:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,5 +37,25 @@ client.on('message', message => {
 });
 
 
+
+client.on('message', message => {
+    if (message.content.startsWith("$avatar")) {
+if(!message.channel.guild) return;
+        var mentionned = message.mentions.users.first();
+    var client;
+      if(mentionned){
+          var client = mentionned; } else {
+          var client = message.author;
+      }
+        const embed = new Discord.RichEmbed()
+                           .addField('Requested by:', "<@" + message.author.id + ">")
+        .setColor(000000)
+        .setImage(`${client.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
