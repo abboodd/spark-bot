d3c7fe3f89309957a4bb0886a419500f058b3a84
abboodd <38141887+abboodd@users.noreply.github.com>
2018-05-22 23:56:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,10 +44,10 @@ client.on("message", message => {
 
         ***__Administrative Orders__***
 **
-『r-kick / كيك』
-『r-ban / باند』
-『r-mute /ميوت』
-『r-unmute /فك الميوت』
+غير مفعل『r-kick / كيك』
+غير مفعل『r-ban / باند』
+غير مفعل『r-mute /ميوت』
+غير مفعل『r-unmute /فك الميوت』
 『r-ct /انشاء روم كتابي』
 『r-cv /انشاء روم صوتي』
 『r-bc /برودكاست』
@@ -320,55 +320,6 @@ m.sendMessage(args)
 })
 }
 });
-
-
-
-var fs = require('fs');
-var Canvas = require('canvas')
-var jimp = require('jimp')
-client.on('guildMemberAdd', member => {
-      
-        if (member.guild.id === "430256496824287233") {
-        var w = ['https://media.discordapp.net/attachments/446874043786723348/447067366959087616/welcome.png'];
-           let Image = Canvas.Image,
-               canvas = new Canvas(401, 202),
-               ctx = canvas.getContext('2d');
-           ctx.patternQuality = 'bilinear';
-           ctx.filter = 'bilinear';
-           ctx.antialias = 'subpixel';
-           ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-           ctx.shadowOffsetY = 2;
-           ctx.shadowBlur = 2;
-           fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-               if (err) return console.log(err);
-               let BG = Canvas.Image;
-               let ground = new Image;
-               ground.src = Background;
-               ctx.drawImage(ground, 0, 0, 401, 202);
-   
-   })
-   
-                   let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
-                   jimp.read(url, (err, ava) => {
-                       if (err) return console.log(err);
-                       ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                           if (err) return console.log(err);
-(edited)
-//AVATAR�
-                              let Avatar = Canvas.Image;
-                              let ava = new Avatar;
-                              ava.src = buf;
-                              ctx.beginPath();
-                              ctx.arc(75, 100, 63, 0, Math.PI*2);
-                                 ctx.closePath();
-                                 ctx.clip();
-                                 ctx.drawImage(ava, 10, 25, 135, 170);                    
-   client.channels.get("ا430280438674620436").sendFile(canvas.toBuffer())
-  client.channels.get("430280438674620436").send(`Welcome ${member} To __${member.guild.name}__`)
-   })})  }
-   });
-
-
 
 
    
