14ccb9d4f8e94c5f21ad3a073cdfa4cefcd9f5cd
abboodd <38141887+abboodd@users.noreply.github.com>
2018-05-23 00:27:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -346,33 +346,14 @@ m.sendMessage(args)
 });
 
 
-  const Discord = require("discord.js");
-const client = new Discord.Client();
 
 
-
-client.on("ready", () => {
-console.log('By : Kyle');
-console.log('ready');
-client.user.setPresence({
-  status: 'dnd',
-  game: { 
-     type: 0,
-     name: '•●{رَمِضّاِنُ كـــًرًيَـــمً ~☾',
-     details: `for help you ^^`,
-     url: 'http://twitch.tv/M7md_Salih',
-     state: `r-help`,
-    application_id: '394600839496466443',
-     assets: {
-        small_image: `444859352093032449`,
-        small_text: '2018',
-        large_image: `444864778465181706`,
-        large_text: `Ramadan Mubarak` }
-
-  }
-    });
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'wlc');
+  if (!channel) return;
+  channel.send(`***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا حياك الله***, ${member}`);
+  
 });
-
    
       
 // THIS  MUST  BE  THIS  WAY
