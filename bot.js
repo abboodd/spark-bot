0464da9ff17ddb7f1eb3d8f153ef8e42b36109f5
abboodd <38141887+abboodd@users.noreply.github.com>
2018-05-23 00:31:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -355,6 +355,16 @@ client.on('guildMemberAdd', member => {
   
 });
    
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag} !`);
+                client.user.setActivity("The World",{type: 'WATCHING'});
+
+});
+
       
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
