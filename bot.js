0eaefe682e55c2464b672b0bbc26537053b38aec
abboodd <38141887+abboodd@users.noreply.github.com>
2018-05-23 00:11:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -170,7 +170,7 @@ client.on("message", message => {
     var prefix = "r-";
  
             var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix r- "clear")) {
+            if (message.content.startsWith(prefix + "clear")) {
    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('? | **ليس لديك صلاحيات**');
         var msg;
         msg = parseInt();
