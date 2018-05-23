dfa6855a47e689da8249bfa66dd566bf7f3117bd
abboodd <38141887+abboodd@users.noreply.github.com>
2018-05-23 17:20:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -360,7 +360,7 @@ client.on('guildMemberAdd', member => {
 
 client.on('ready', function(){
     var ms = 100000 ;
-    var setGame = [`r-help Servers  `,`r-invite Users`];
+    var setGame = [`r-help   `,`r-invite `];
     var i = -1;
     var j = 0;
     setInterval(function (){
